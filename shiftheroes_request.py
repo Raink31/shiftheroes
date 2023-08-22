357247857ff006f92e09f2cd9eae04e97a837840
Yohan Baes <111435158+Raink31@users.noreply.github.com>
2023-08-22 23:31:31 +0200

    Add files via upload

--- shiftheroes_request.py
+++ shiftheroes_request.py
@@ -1,14 +1,28 @@
 import requests
+import time
 
 headers = {
     'Authorization': 'Bearer 577e9aec2586096664daebb486df09f9',
 }
 
+
 # Lister les plannings
-response = requests.get('https://shiftheroes.fr/api/v1/plannings', headers=headers)
+liste_initiale = requests.get('https://shiftheroes.fr/api/v1/plannings?type=daily', headers=headers).json()
+nouvelle_liste = requests.get('https://shiftheroes.fr/api/v1/plannings?type=daily', headers=headers).json()
 
-# Lister les creneaux
-response = requests.get('https://shiftheroes.fr/api/v1/plannings/rwf10y/shifts', headers=headers)
 
-# Reserver un creneau
-response = requests.post('https://shiftheroes.fr/api/v1/plannings/rwf10y/shifts/D6FKomo/reservations', headers=headers)
+# Détecter quand un nouveau planning est dispobible
+while liste_initiale == nouvelle_liste:
+    nouvelle_liste = requests.get('https://shiftheroes.fr/api/v1/plannings?type=daily', headers=headers).json()
+    time.sleep(1)
+
+print("Nouveau planning disponible!")
+
+
+# Lister les creneaux avec la variable précédente
+all_creneaux = requests.get(f'https://shiftheroes.fr/api/v1/plannings/{nouvelle_liste[0]["id"]}/shifts', headers=headers).json()
+
+
+# Reserver tous les creneaux
+for item in all_creneaux :
+    requests.post(f'https://shiftheroes.fr/api/v1/plannings/{nouvelle_liste[0]["id"]}/shifts/{item["id"]}/reservations', headers=headers)
